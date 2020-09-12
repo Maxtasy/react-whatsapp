02326ec4dae0c688993271c12228363bf41ce870
maxtasy <maxtasy88@web.de>
2020-09-12 23:08:21 +0200

    Add Conversations Panel

--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -16,20 +16,20 @@ export default function NewContactModal({ closeModal }) {
 
     return (
         <>
-            <Modal.Header closeButton>Create Contact</Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group>
-                        <Form.Label>ID</Form.Label>
-                        <Form.Control type="text" ref={idRef} required />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" ref={nameRef} required />
-                    </Form.Group>
-                    <Button type="submit">Create</Button>
-                </Form>
-            </Modal.Body>
+          <Modal.Header closeButton>Create Contact</Modal.Header>
+          <Modal.Body>
+              <Form onSubmit={handleSubmit}>
+                  <Form.Group>
+                      <Form.Label>ID</Form.Label>
+                      <Form.Control type="text" ref={idRef} required />
+                  </Form.Group>
+                  <Form.Group>
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control type="text" ref={nameRef} required />
+                  </Form.Group>
+                  <Button type="submit">Create</Button>
+              </Form>
+          </Modal.Body>
         </>
     )
 }
